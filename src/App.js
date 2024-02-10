878cd36eaf0873c106f9ea5830cfea7d96278475
nidhimp92 <nidhimp92@gmail.com>
2024-02-10 14:42:12 +0100

    new code

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Learn React New Push to Git
         </a>
       </header>
     </div>
